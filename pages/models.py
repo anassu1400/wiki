ff5fce72ce5b556fe01d20ed93845841b210513a
anassu1400 <anassu1400@gmail.com>
2019-02-12 12:30:00 +0300

    Anas, sitah

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=50)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+	def get_absolute_url(self):
+		return "/detail/%d" % self.id
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Page
 # Create your views here.
+def list_view(request):
+	context ={
+		'my_page': Page.objects.all()
+	}
+	return render(request, 'list.html', context)
+
+def detail_view(request, page_id):
+	context ={
+		'my_page': Page.objects.get(id=page_id)
+	}
+	return render(request, 'detail.html', context)
